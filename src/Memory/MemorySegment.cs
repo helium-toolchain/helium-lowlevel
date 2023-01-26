b4ec7dbf62d492d5013ad690ae319c569751e871
akiraveliara <akiraveliara@outlook.com>
2023-01-26 19:28:51 +0100

    MemorySegment, representing a part of a linked chain of memory segments

---
+++ src/Memory/MemorySegment.cs
@@ -0,0 +1,34 @@
+﻿namespace Helium.LowLevel.Memory;
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+/// <summary>
+/// Represents a segment of memory, with a reference to the next segment in the chain.
+/// Obtained from a memory allocator.
+/// </summary>
+internal unsafe ref struct MemorySegment
+{
+    // a pointer to the memory region this represents. must not be null.
+    // this could just as well be represented as a span, but that makes MemorySegment* quite worrying
+    private readonly void* ptr;
+    private readonly UIntPtr length;
+
+    public MemorySegment* NextSegment { get; set; }
+
+    public Boolean HasNextSegment()
+    {
+        return this.NextSegment == null;
+    }
+
+    // exposes the memory as span
+    public Span<Byte> Span
+    {
+        get => MemoryMarshal.CreateSpan
+        (
+            ref Unsafe.AsRef<Byte>(ptr),
+            (Int32)length
+        );
+    }
+}
